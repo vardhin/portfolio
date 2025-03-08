d11f4cd5731c154f8aecda6a96c69ecf37ef6b5f
vardhin <gamidisurya.vardhin2022@vitstudent.ac.in>
2025-03-08 07:24:11 +0530

    try

--- src/lib/components/CloudRenderer.svelte
+++ src/lib/components/CloudRenderer.svelte
@@ -1020,11 +1020,6 @@
         normalizedMousePosition.x = ((event.clientX - rect.left) / rect.width) * 2 - 1;
         normalizedMousePosition.y = -((event.clientY - rect.top) / rect.height) * 2 + 1;
     };
-
-
-    const onTouchEnd = () => {
-        isDragging = false;
-    };
 </script>
   
 <div class="main-container">
